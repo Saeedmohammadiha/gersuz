a172d3f1d10a32a8b59f8dc78f7636d194c360b2
saeed mohammdiha <saeed1440124@gmail.com>
2024-04-16 12:08:03 +0330

    add the languages api

---
+++ src/app/main/admin/languagesApi.ts
@@ -0,0 +1,47 @@
+import { apiService as api } from 'app/store/apiService';
+
+export const addTagTypes = ['languages', 'change'] as const;
+export const urls = {
+	GetAllLanguage: 'api/Language/GetAllLanguage/',
+	ChangeLanguage: 'api/Language/ChangeLanguage/'
+};
+
+export type Language = {
+	id: string;
+	languageTitle: string;
+};
+
+const LanguageApi = api
+	.enhanceEndpoints({
+		addTagTypes
+	})
+	.injectEndpoints({
+		endpoints: (build) => ({
+			getAllLanguage: build.query<LanguageApiResponse, GetLanguagesApiArg>({
+				query: () => ({ url: urls.GetAllLanguage }),
+				providesTags: ['languages']
+			}),
+			changeLanguage: build.mutation<ChangeLanguagesApiResponse, ChangeLanguagesApiArg>({
+				query: (faqId) => ({
+					url: `/api/FAQ/DeleteFAQ/${faqId}`,
+					method: 'DELETE'
+				}),
+				invalidatesTags: ['change']
+			})
+		}),
+		overrideExisting: false
+	});
+
+export default LanguageApi;
+
+export const { useGetAllLanguageQuery, useChangeLanguageMutation } = LanguageApi;
+
+export type ApiType = {
+	[LanguageApi.reducerPath]: ReturnType<typeof LanguageApi.reducer>;
+};
+
+export type LanguageApiResponse = Language;
+
+export type GetLanguagesApiArg = void;
+export type ChangeLanguagesApiArg = Omit<Language, 'languageTitle'>;
+export type ChangeLanguagesApiResponse = void;
